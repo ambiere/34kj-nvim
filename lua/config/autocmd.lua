434cca3eb8c71536b636e362169803f22b0bbb2f
zhid0399123 <zhid0399123@gmail.com>
2025-05-05 13:46:24 +0300

    feat: autocmd plugin

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,31 @@
+local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
+local whitespace_group = vim.api.nvim_create_augroup("WhitespaceAutogroup", {})
+local semicolon_group = vim.api.nvim_create_augroup("SemicolonAutogroup", {})
+
+-- remove whitespace at the end of the line
+-- before buffer has been written
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = whitespace_group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
+
+-- remove semicolon in js/tsx
+-- before buffer has been written
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = semicolon_group,
+	pattern = "*.{js,jsx}",
+	command = [[%s/;$//e]],
+})
+
+-- highlight the yanked text for n-time
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = highlight_yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
